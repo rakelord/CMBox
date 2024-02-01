fe38058e65ad5033689be5ec7ef7bc16926b3a88
rakelord <magnus.schollin@rts.se>
2024-02-01 15:12:25 +0100

    Page structure

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+function getPage(btn){
+    let pagename = $(btn).attr('pagename');
+    alert(pagename);
+    $('mainpage').html($.get("/pages/"+pagename+".html"));
+}
+
 $(document).ready(function(){
     /* FIX Navigation Bar On Load */
     $('.navparent').each(function(){
-        let newBtnText = '<svg xmlns="http://www.w3.org/2000/svg" fill="grey" viewBox="4 5 15 15" height="12"><title>arrow-down-thick</title><path d="M10,4H14V13L17.5,9.5L19.92,11.92L12,19.84L4.08,11.92L6.5,9.5L10,13V4Z" /></svg><div style="display: inline-block;margin-left: 8px">'+this.textContent+'</div><div style="width: 22px;display: inline-block"></div>';
+        let newBtnText = this.textContent+'<svg style="padding: 0px 8px" xmlns="http://www.w3.org/2000/svg" fill="lightgrey" viewBox="4 5 15 15" height="12"><title>arrow-down-thick</title><path d="M10,4H14V13L17.5,9.5L19.92,11.92L12,19.84L4.08,11.92L6.5,9.5L10,13V4Z" /></svg><div style="display: inline-block;margin-left: 8px">';
         $(this).html(newBtnText)
-    });
-    $('.navnormal').each(function(){
-        $(this).html('<div style="width: 20px;display: inline-block"></div>'+this.textContent+'<div style="width: 22px;display: inline-block"></div>');
     });
     $('.navparent').next().hide();
 
