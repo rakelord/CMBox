a0b6db95fbb810280b588ad676d126d5fb7ddea9
admmagsch <magnus.schollin@gmail.com>
2024-02-02 23:15:12 +0100

    changed website icons

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
     /* FIX Navigation Bar On Load */
     $('.navparent').each(function(){
-        let newBtnText = this.textContent+'<svg style="padding: 0px 8px" xmlns="http://www.w3.org/2000/svg" fill="lightgrey" viewBox="4 5 15 15" height="12"><title>arrow-down-thick</title><path d="M10,4H14V13L17.5,9.5L19.92,11.92L12,19.84L4.08,11.92L6.5,9.5L10,13V4Z" /></svg><div style="display: inline-block;margin-left: 8px">';
+        let newBtnText = this.innerHTML+'<span class="material-symbols-outlined">expand_more</span>';
         $(this).html(newBtnText)
     });
     $('.navparent').next().hide();
@@ -17,12 +17,12 @@ $(document).ready(function(){
         if ($(this).next().is(':visible')){
             $(this).next().slideToggle(200);
             $(this).removeClass('listopen');
-            $(this).find('svg').animate({rotate: '360deg'},100);
+            $(this).find('span').animate({rotate: '360deg'},100);
         }
         else {
             $(this).next().slideToggle(200);
             $(this).addClass('listopen');
-            $(this).find('svg').animate({rotate: '180deg'},100);
+            $(this).find('span').animate({rotate: '180deg'},100);
         }
     });
 
