ab2ab112993a3941298c00df30d7fa10b523efca
admmagsch <magnus.schollin@gmail.com>
2024-02-06 19:59:04 +0100

    added a template appsettings file

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c087e371a3504668a72f864759b52c0ccf1f004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b159a49a746420eda71477d7b47f0d4e6b73622a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
