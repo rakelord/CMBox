bead5eb6d1220a3ec359b97651ab58e99246850b
admmagsch <magnus.schollin@gmail.com>
2024-02-07 11:36:27 +0100

    wip

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d980522f125caf7c69f94332a877e26b7adf2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2752425fcd5ced1054c5cbd18faacb6f6616320a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
